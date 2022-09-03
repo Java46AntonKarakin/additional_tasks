962d0a7407cb1c3386022aa2612a88beb3c19ff9
volnykot <volnykot3@gmail.com>
2022-09-03 19:22:52 +0300

    adds ADD#10

---
+++ src/telrun/utils/MyPredicate.java
@@ -0,0 +1,27 @@
+package telrun.utils;
+
+public interface MyPredicate<T> {
+
+	boolean test(T t);
+
+	default MyPredicate<T> negate() {
+		return t -> !test(t);
+	}
+
+	default MyPredicate<T> or(MyPredicate<T> other) {
+		return t -> test(t) || other.test(t);
+	}
+
+	default MyPredicate<T> and(MyPredicate<T> other) {
+		return t -> test(t) && other.test(t);
+	}
+
+	static <T> MyPredicate<T> isEqual(Object targetRef) {
+		return t -> targetRef.equals(t);
+	}
+
+	static <T> MyPredicate<T> not(MyPredicate<T> target) {
+		return t -> !target.test(t);
+	}
+
+}
---
+++ src/telrun/utils/PublicNegator.java
@@ -0,0 +1,16 @@
+package telrun.utils;
+
+//public class PublicNegator <T> implements MyPredicate<T> {
+//	MyPredicate <T> originalPredicate;
+//	
+//	public PublicNegator(MyPredicate <T> originalPredicate) {
+//		this.originalPredicate = originalPredicate;
+//	}
+//
+//	@Override
+//	public boolean test (T t) {
+//		return !originalPredicate.test(t);
+//	}
+//	
+//
+//}

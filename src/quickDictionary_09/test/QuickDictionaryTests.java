fb5101b6ebf1659d7fa258eaac87c53a6b7f8d0f
volnykot <volnykot3@gmail.com>
2022-09-16 21:00:42 +0300

    ADD-09 final update

--- src/quickDictionary_09/test/QuickDictionaryTests.java
+++ src/quickDictionary_09/test/QuickDictionaryTests.java
@@ -15,13 +15,7 @@ import quickDictionary_09.QuickDictArrayList;
 import quickDictionary_09.QuickDictNodes;
 
 class QuickDictionaryTests {
-
-	/* solution with two ArrayLists */
-	Parent dicArrList = new QuickDictArrayList();
-
-	/* solution with Node structure */
-	Parent dicNodes = new QuickDictNodes();
-
+	
 	static String mammals = "cat, gog, whale";
 	static String birds = "eagle, chicken, papugay";
 	static String fish = "shark, clownfish, catfish";
@@ -29,29 +23,28 @@ class QuickDictionaryTests {
 
 	static String[] arrayOfValues = { mammals, birds, fish, reptiles };
 	static String[] arrayOfKeys = { "MAMMALS", "BIRDS", "FISH", "REPTILES" };
-	
-	
+
 	/*
-	 *  "Stream" style  
-	 *  values should be > 2000 otherways tests will take more than 10 sec per iteration		
+	 * "Stream" style values should be > 2000 otherways tests will take more than 10
+	 * sec per iteration
 	 */
-	
+
 //	static String[] valuesPT = getPseudoWordsArray(2000);
 //	static String[] keysPT = getPseudoWordsArray(2000);
-	
-	/* "Double for" style   	 */
-	static String[] valuesPT = getPseudoWordsArrayOld(20000);
-	static String[] keysPT = getPseudoWordsArrayOld(20000);
-	
+
+	/* "Double for" style */
+	static String[] valuesPT = getPseudoWordsArrayOld(10000);
+	static String[] keysPT = getPseudoWordsArrayOld(10000);
+
 	static final int N_REPEATS = 5;
 
 	@Test
 	void dictionaryTest() {
-		dictFunctionalTest (dicArrList);
-		dictFunctionalTest (dicNodes);
+		dictFunctionalTest(new QuickDictArrayList());
+		dictFunctionalTest(new QuickDictNodes());
 	}
-	
-	private void dictFunctionalTest (Parent dictionary) {
+
+	private void dictFunctionalTest(Parent dictionary) {
 		for (int i = 0; i < arrayOfKeys.length; i++) {
 			String put = dictionary.put(arrayOfKeys[i], arrayOfValues[i]);
 			String get = dictionary.get(arrayOfKeys[i]);
@@ -59,42 +52,47 @@ class QuickDictionaryTests {
 			assertTrue(get.equals(arrayOfValues[i]));
 			assertEquals(get, dictionary.put(arrayOfKeys[i], arrayOfValues[i]));
 		}
+		assertNull(dictionary.get("SOMETHING"));
 	}
 
 	@Test
 	void dictionaryPerformanceTest() {
-		System.out.println("*".repeat(10) + "dicArrList" + "*".repeat(10));
-		performanceTest(dicArrList);
-		
-		System.out.println("*".repeat(10) + "dicNodes" + "*".repeat(10));
-		performanceTest(dicNodes);
-	}
-	
-	private static void performanceTest(Parent dictionary) {
-		
 		int iterationCounter = 1;
+		System.out.println("*".repeat(10) + " dictArrList - performanceTest " + "*".repeat(10));
 		for (int j = 0; j < N_REPEATS; j++) {
-			
-			var startOfIteration = LocalDateTime.now();
-			
-			for (int i = 0; i < keysPT.length; i++) {
-				dictionary.put(keysPT[i], valuesPT[i]);
-			}
-			
-			var putTestLength = ChronoUnit.MILLIS.between(startOfIteration, LocalDateTime.now());
-			var startOfGetTest = LocalDateTime.now();
-			
-			for (int i = 0; i < keysPT.length; i++) {
-				dictionary.get(keysPT[i]);
-			}
-			
-			var getTestLength = ChronoUnit.MILLIS.between(startOfGetTest, LocalDateTime.now());
-			var commonLength = putTestLength + getTestLength;
-			System.out.printf("iteration #%d:     put = %s ms, get = %s ms, common time = %s ms;\n",
-					iterationCounter++, putTestLength, getTestLength, commonLength );
+			System.out.printf("iteration #%d:     ", iterationCounter++);
+			performanceTest(new QuickDictArrayList());
+		}
+
+		System.out.println("*".repeat(10) + " dictNodes - performanceTest " + "*".repeat(10));
+		iterationCounter = 1;
+		for (int j = 0; j < N_REPEATS; j++) {
+			System.out.printf("iteration #%d:     ", iterationCounter++);
+			performanceTest(new QuickDictNodes());
 		}
 	}
-	
+
+	private static void performanceTest(Parent dictionary) {
+		var startOfIteration = LocalDateTime.now();
+
+		for (int i = 0; i < keysPT.length; i++) {
+			dictionary.put(keysPT[i], valuesPT[i]);
+		}
+
+		var putTestLength = ChronoUnit.MILLIS.between(startOfIteration, LocalDateTime.now());
+		var startOfGetTest = LocalDateTime.now();
+
+		for (int i = 0; i < keysPT.length; i++) {
+			dictionary.get(keysPT[i]);
+		}
+
+		var getTestLength = ChronoUnit.MILLIS.between(startOfGetTest, LocalDateTime.now());
+		var commonLength = putTestLength + getTestLength;
+		System.out.printf("put = %s ms, get = %s ms, common time = %s ms;\n", putTestLength, getTestLength,
+				commonLength);
+
+	}
+
 	private static String[] getPseudoWordsArray(int numberOfElements) {
 		StringBuilder sb = new StringBuilder();
 		String[] res = new String[numberOfElements];
@@ -103,16 +101,16 @@ class QuickDictionaryTests {
 	}
 
 	private static String[] getPseudoWordsArrayOld(int numberOfElements) {
-		
+
 		String[] res = new String[numberOfElements];
-		
+
 		StringBuilder sb = new StringBuilder();
-		
+
 		for (int i = 0; i < numberOfElements; i++) {
-			int length =getRandomLength();
-			
+			int length = getRandomLength();
+
 			for (int j = 0; j < length; j++) {
-				
+
 				char randomChar = getRandomChar();
 				sb.append(randomChar);
 			}
@@ -121,19 +119,17 @@ class QuickDictionaryTests {
 		}
 		return res;
 	}
-	
+
 	private static String getWord(StringBuilder sb) {
-		IntStream.range(0, getRandomLength())
-		.map(x -> getRandomChar())
-		.forEach(x -> sb.append((char)x));
+		IntStream.range(0, getRandomLength()).map(x -> getRandomChar()).forEach(x -> sb.append((char) x));
 		return sb.toString();
 	}
-		
-    private static int getRandomLength () {
-    	return (int)(Math.random() * ((20 - 1) + 1) + 1);
-    }
-    
-    private static char getRandomChar () {
-    	return (char)((Math.random() * ((90 - 65) + 1)) + 65);
-    }
+
+	private static int getRandomLength() {
+		return (int) (Math.random() * ((20 - 1) + 1) + 1);
+	}
+
+	private static char getRandomChar() {
+		return (char) ((Math.random() * ((90 - 65) + 1)) + 65);
+	}
 }
